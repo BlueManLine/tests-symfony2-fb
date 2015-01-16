ddadda05cfb9f5bcc81b7c161d1d7b7b8b40449e
Szymon Bluma <szymon.bluma@polcode.net>
2015-01-16 09:06:55 +0100

    FOSUserBundle - with custom pages, redirections

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,16 +2,20 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\LoginType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/app/example", name="homepage")
+     * @Route("/", name="dashboard")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+
+
         return $this->render('default/index.html.twig');
     }
 }
